c09c81742a7c50c8d307a0fafa5ebfbd68dee3a3
Ajay5201 <ajay123mohan@gmail.com>
2023-05-09 13:28:34 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,38 @@
-import Head from "next/head";
-import axios from "axios";
-import Image from "next/image";
+import Featured from '../components/Featured';
+import Head from 'next/head';
+import Image from 'next/image';
+import PizzaList from '../components/PizzaList';
+import axios from 'axios';
+import styles from '../styles/Home.module.css';
 
-import Featured from "../components/Featured";
-import PizzaList from "../components/PizzaList";
-import styles from "../styles/Home.module.css";
-
-export default function Home({pizzaList,admin}) {
-
-  
+export default function Home({ pizzaList, admin }) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Pizza Restaurant in Newyork</title>
-        <meta name="description" content="Best pizza shop in town" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Best pizza shop in town' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Featured/>
+      <Featured />
       {admin && <span></span>}
-      <PizzaList pizzaList={pizzaList}/>
+      <PizzaList pizzaList={pizzaList} />
     </div>
   );
 }
 
-export const getServerSideProps = async(ctx) =>{
-  const myCookie = ctx.req?.cookies || "";
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || '';
   let admin = false;
 
-  if (myCookie.token === process.env.TOKEN) {
+  if (myCookie.token === 'Wdw4CV||663Z{p3|ZXtP%0k6Ejj;F') {
     admin = true;
   }
 
-  const res =await axios.get("http://localhost:3000/api/products");
-  return{
-    props:{
-      pizzaList:res.data,
-      admin
-     
-    } 
-  }
+  const res = await axios.get('http://localhost:3000/api/products');
+  return {
+    props: {
+      pizzaList: res.data,
+      admin,
+    },
+  };
 };
--- util/mongo.js
+++ util/mongo.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const MONGO_URL = "mongodb+srv://Abinaya:apple@cluster0.603c30o.mongodb.net/pizza?retryWrites=true&w=majority"
+const MONGO_URL =
+  'mongodb+srv://dummytestingdummytesting:ajay@cluster0.qxmdht3.mongodb.net/pizza?retryWrites=true&w=majority';
 
 if (!MONGO_URL) {
   throw new Error(
     'Please define the MONGO_URL environment variable inside .env.local'
-  )
+  );
 }
 
 /**
@@ -13,28 +14,28 @@ if (!MONGO_URL) {
  * in development. This prevents connections growing exponentially
  * during API Route usage.
  */
-let cached = global.mongoose
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
   if (cached.conn) {
-    return cached.conn
+    return cached.conn;
   }
 
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
-    }
+    };
 
     cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
-      return mongoose
-    })
+      return mongoose;
+    });
   }
-  cached.conn = await cached.promise
-  return cached.conn
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
-export default dbConnect
+export default dbConnect;
